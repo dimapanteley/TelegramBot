8dde4cf34ebc8c4afc19b15d12d1062a80358d82
Dima <dimapanteleyvrach@gmail.com>
2023-09-13 15:01:00 +0300

    готово

---
+++ src/main/java/pro/sky/telegrambot/model/NotificationTack.java
@@ -0,0 +1,21 @@
+package pro.sky.telegrambot.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+@Data
+@Table(name = "notification_task")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+public class NotificationTack {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long taskId;
+    private long chatId;
+    private String textMessage;
+    private LocalDateTime dateTime;
+}
+
