29b7a8ebbbc966f345aa839a8bb80b5f0c00ff3c
Dima <dimapanteleyvrach@gmail.com>
2023-09-25 16:59:41 +0300

    Ошибка

--- src/main/java/pro/sky/telegrambot/configuration/TelegramBotConfiguration.java
+++ src/main/java/pro/sky/telegrambot/configuration/TelegramBotConfiguration.java
@@ -1,15 +1,29 @@
 package pro.sky.telegrambot.configuration;
 
 import com.pengrad.telegrambot.TelegramBot;
+import com.pengrad.telegrambot.TelegramException;
+import com.pengrad.telegrambot.model.BotCommand;
 import com.pengrad.telegrambot.model.DeleteMyCommands;
+import com.pengrad.telegrambot.model.botcommandscope.BotCommandScope;
+import com.pengrad.telegrambot.model.botcommandscope.BotCommandScopeDefault;
+import com.pengrad.telegrambot.request.SetMyCommands;
+import lombok.Data;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import pro.sky.telegrambot.listener.TelegramBotUpdatesListener;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
 @Configuration
 public class TelegramBotConfiguration {
+    private Logger logger = LoggerFactory.getLogger(TelegramBotUpdatesListener.class);
 
-    @Value("${telegram.bot.token}")
+    @Value("6667017288:AAEa3J0FLXmJ13mSvvQpbManrDTopy94N3Y")
     private String token;
 
     @Bean
--- src/main/resources/liqubase/scripts/notification_task.sql
+++
@@ -1,9 +0,0 @@
--- liquibase formatted sql
-
--- changeset Dpanteley:1
-create table notification_task(
-                                  task_id serial primary key ,
-                                  chat_id serial not null ,
-                                  text_message text not null ,
-                                  date_time timestamp not null
-);
